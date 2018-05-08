05c663bc082f45ffce53e4c8f89818535fddf0f0
Micah Alcorn <mail@micahalcorn.com>
2018-05-08 17:10:50 -0500

    Truncatable address

--- src/components/truncatable-address.js
+++ src/components/truncatable-address.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
 
+/*
+ * ETH addresses should not be truncated in cases
+ * where a transaction is about to be consummated
+ * or where the current user is being identified.
+ * This component should only be used in cases where
+ * there is more than one address shown in close
+ * proximity to another.
+*/
+
 class TruncatableAddress extends Component {
   render() {
-    const { address } = this.props
+    const { address, className } = this.props
 
     return (
-      <div className="address d-flex justify-content-around">
-        <div>{address.slice(0, address.length/2)}</div>
-        <div>{address.slice(address.length/2)}</div>
+      <div className={`address d-flex justify-content-around${className ? ` ${className}` : ''}`}>
+        <div>{address.slice(0, address.length / 2)}</div>
+        <div>{address.slice(address.length / 2)}</div>
       </div>
     )
   }
